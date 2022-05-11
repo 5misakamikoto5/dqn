bab380b197b1b29c10943fa1eb8c4ed763ebdbb7
5misakamikoto5 <90431728+5misakamikoto5@users.noreply.github.com>
2022-05-11 22:00:54 +0800

    zuihougai

--- agent_dir/agent_pg.py
+++ agent_dir/agent_pg.py
@@ -6,58 +6,26 @@ import torch
 from pathlib import Path
 from tensorboardX import SummaryWriter
 from torch import nn, optim
+import nn.functional as F
 from agent_dir.agent import Agent
 
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 class PGNetwork(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super(PGNetwork, self).__init__()
         ##################
         # YOUR CODE HERE #
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, output_size)
         ##################
         pass
 
     def forward(self, inputs):
         ##################
         # YOUR CODE HERE #
-        ##################
-        pass
-
-
-class ReplayBuffer:
-    def __init__(self, buffer_size):
-        """
-        Trajectory buffer. It will clear the buffer after updating.
-        """
-        ##################
-        # YOUR CODE HERE #
-        ##################
-        pass
-
-    def __len__(self):
-        ##################
-        # YOUR CODE HERE #
-        ##################
-        pass
-
-    def push(self, *transition):
-        ##################
-        # YOUR CODE HERE #
-        ##################
-        pass
-
-    def sample(self, batch_size):
-        """
-        Sample all the data stored in the buffer
-        """
-        ##################
-        # YOUR CODE HERE #
-        ##################
-        pass
-
-    def clean(self):
-        ##################
-        # YOUR CODE HERE #
+        x = F.relu(self.fc1(inputs))
+        x = F.softmax(self.fc2(x),dim = 1)
         ##################
         pass
 
@@ -71,6 +39,17 @@ class AgentPG(Agent):
         super(AgentPG, self).__init__(env)
         ##################
         # YOUR CODE HERE #
+        self.env = env
+        self.h = self.env.observation_space.shape[0]  # observation_dim,input size
+        self.w = self.env.observation_space.shape[1]
+        self.c = self.env.observation_space.shape[2]
+        self.action_dim = self.env.action_space.n  # output_size
+        self.policy_net = PGNetwork(self.h, args.hidden_size,
+                                    self.action_dim).to(device)
+        self.optimizer = torch.optim.Adam(self.policy_net.parameters(),
+                                          lr=args.lr)  # 使用Adam优化器
+        self.gamma = self.gamma  # 折扣因子
+        # self.device = device
         ##################
         pass
 
@@ -86,12 +65,28 @@ class AgentPG(Agent):
         ##################
         pass
 
-    def train(self):
+    def train(self,transition_dict):
         """
         Implement your training algorithm here
         """
         ##################
         # YOUR CODE HERE #
+        reward_list = transition_dict['rewards']
+        state_list = transition_dict['states']
+        action_list = transition_dict['actions']
+
+        G = 0
+        self.optimizer.zero_grad()
+        for i in reversed(range(len(reward_list))):  # 从最后一步算起
+            reward = reward_list[i]
+            state = torch.tensor([state_list[i]],
+                                 dtype=torch.float).to(self.device)
+            action = torch.tensor([action_list[i]]).view(-1, 1).to(self.device)
+            log_prob = torch.log(self.policy_net(state).gather(1, action))
+            G = self.gamma * G + reward
+            loss = -log_prob * G  # 每一步的损失函数
+            loss.backward()  # 反向传播计算梯度
+        self.optimizer.step()  # 梯度下降
         ##################
         pass
 
@@ -103,8 +98,13 @@ class AgentPG(Agent):
         """
         ##################
         # YOUR CODE HERE #
+        obs = torch.tensor([observation], dtype=torch.float).to(self.device)
+        probs = self.policy_net(obs)
+        action_dist = torch.distributions.Categorical(probs)
+        action = action_dist.sample()
+        return action.item()
         ##################
-        pass
+        # pass
 
     def run(self):
         """
